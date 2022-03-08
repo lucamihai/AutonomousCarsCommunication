68ec83df1f3c17de77af3e9e6300a736e9ac3c1c
Mihai Luca <luca.mihai97@yahoo.com>
2022-03-08 21:10:12 +0200

    Add DI layer

---
+++ AutonomousCarsCommunication.DI/DependencyResolver.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using AutonomousCarsCommunication.BusinessLogic;
+using AutonomousCarsCommunication.BusinessLogic.Contracts;
+using AutonomousCarsCommunication.DataAccess;
+using AutonomousCarsCommunication.DataAccess.Contracts;
+using AutonomousCarsCommunication.Repositories;
+using AutonomousCarsCommunication.Repositories.Contracts;
+using AutonomousCarsCommunication.Repositories.Contracts.Mappers;
+using AutonomousCarsCommunication.Repositories.Mappers;
+using AutonomousCarsCommunication.Services;
+using AutonomousCarsCommunication.Services.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutonomousCarsCommunication.DI
+{
+    [ExcludeFromCodeCoverage]
+    public static class DependencyResolver
+    {
+        public static ServiceCollection GetServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddScoped<ICarInteractionBusinessLogic, CarInteractionBusinessLogic>();
+
+            services.AddScoped<ICarMapper, CarMapper>();
+            services.AddScoped<IEventMapper, EventMapper>();
+
+            services.AddScoped<ICarRepository, CarRepository>();
+            services.AddScoped<IEventRepository, EventRepository>();
+
+            services.AddScoped<IDataContext, DataContext>();
+
+            services.AddScoped<IAuthorizationService, FakeAuthorizationService>();
+            services.AddScoped<ILocationService, LocationService>();
+
+            return services;
+        }
+    }
+}
--- AutonomousCarsCommunication.GUI/MainWindow.cs
+++ AutonomousCarsCommunication.GUI/MainWindow.cs
@@ -1,10 +1,19 @@
+using AutonomousCarsCommunication.BusinessLogic.Contracts;
+using AutonomousCarsCommunication.DI;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace AutonomousCarsCommunication.GUI
 {
     public partial class MainWindow : Form
     {
+        private readonly ICarInteractionBusinessLogic carInteractionBusinessLogic;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var serviceProvider = DependencyResolver.GetServices().BuildServiceProvider();
+            carInteractionBusinessLogic = serviceProvider.GetRequiredService<ICarInteractionBusinessLogic>();
         }
     }
 }
